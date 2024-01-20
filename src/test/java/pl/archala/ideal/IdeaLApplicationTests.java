893f5ee2992e12b0738038d5a27535faceccee83
darch <damianarchala@gmail.com>
2024-01-20 16:11:11 +0100

    Init project

---
+++ src/test/java/pl/archala/ideal/IdeaLApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.archala.ideal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
