c39fcf211613c57c05808477958bfe7457acdfb2
darch <damianarchala@gmail.com>
2024-01-23 23:56:04 +0100

    Use GetRealizationDTO instead of GetSimpleRealizationDTO

--- src/main/java/pl/archala/ideal/dto/realization/GetRealizationDTO.java
+++ src/main/java/pl/archala/ideal/dto/realization/GetRealizationDTO.java
@@ -2,7 +2,6 @@ package pl.archala.ideal.dto.realization;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import pl.archala.ideal.dto.idea.GetIdeaDTO;
 import pl.archala.ideal.entity.Realization;
 
 import java.time.LocalDateTime;
@@ -14,12 +13,12 @@ public class GetRealizationDTO {
     private final Long id;
     private final String content;
     private final LocalDateTime created;
-    private final GetIdeaDTO ideaDTO;
+    private final Long ideaId;
 
     public GetRealizationDTO(Realization realization) {
         this.id = realization.getId();
         this.content = realization.getContent();
         this.created = realization.getCreated();
-        this.ideaDTO = new GetIdeaDTO(realization.getIdea());
+        this.ideaId = realization.getIdea().getId();
     }
 }
