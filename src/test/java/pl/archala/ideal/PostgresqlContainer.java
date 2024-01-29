795b0bf5e14a221319bc51f0a1277df122eb26b5
darch <damianarchala@gmail.com>
2024-01-29 23:36:00 +0100

    Add first integration test

---
+++ src/test/java/pl/archala/ideal/PostgresqlContainer.java
@@ -0,0 +1,25 @@
+package pl.archala.ideal;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class PostgresqlContainer {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:16")
+                    .withDatabaseName("postgres_db")
+                    .withPassword("admin")
+                    .withUsername("admin");
+
+    @DynamicPropertySource
+    public static void containerConfig(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+}
