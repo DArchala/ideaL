899f5002dbafd2631f9a11f59b13fd45ecc2784e
darch <damianarchala@gmail.com>
2024-03-13 19:31:53 +0100

    Migrate ErrorResponse from class to record

--- src/main/java/pl/archala/ideal/error/ErrorResponse.java
+++ src/main/java/pl/archala/ideal/error/ErrorResponse.java
@@ -4,27 +4,16 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
+import pl.archala.ideal.serial.HttpStatusDeserializer;
+import pl.archala.ideal.serial.HttpStatusSerializer;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-@Getter
-@NoArgsConstructor
-public class ErrorResponse {
-
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    private LocalDateTime occurred;
-    private List<String> reasons;
-    private HttpStatus status;
-
-    public ErrorResponse(List<String> reasons, HttpStatus status) {
-        this.occurred = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-        this.reasons = reasons;
-        this.status = status;
-    }
+public record ErrorResponse(@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+                            @JsonSerialize(using = LocalDateTimeSerializer.class) LocalDateTime occurred,
+                            List<String> reasons,
+                            @JsonDeserialize(using = HttpStatusDeserializer.class)
+                            @JsonSerialize(using = HttpStatusSerializer.class) HttpStatus status) {
 }
