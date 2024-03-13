feee5c3593ccee2e13d79e4feb9996de1c0a60ef
darch <damianarchala@gmail.com>
2024-03-13 19:32:24 +0100

    Update IdeasControllerTest

--- src/test/java/pl/archala/ideal/controller/IdeasControllerTest.java
+++ src/test/java/pl/archala/ideal/controller/IdeasControllerTest.java
@@ -4,11 +4,16 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import pl.archala.ideal.PostgresqlContainer;
 import pl.archala.ideal.dto.idea.AddIdeaDTO;
 import pl.archala.ideal.dto.idea.GetIdeaDTO;
+import pl.archala.ideal.enums.IdeaCategory;
+import pl.archala.ideal.error.ErrorResponse;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -56,6 +61,40 @@ class IdeasControllerTest extends PostgresqlContainer {
         assertEquals(simpleContent, actualGetIdeaDTO.content());
         assertNull(actualGetIdeaDTO.realizationsIds());
         assertNull(actualGetIdeaDTO.commentsIds());
+    }
+
+    @Test
+    void shouldReturnCategoriesList() {
+        //given
+        List<IdeaCategory> expectedCategories = List.of(IdeaCategory.values());
+
+        //when
+        List<IdeaCategory> actualCategories = webTestClient.get().uri("/api/idea/categories").exchange()
+                .expectStatus().isOk()
+                .expectBodyList(IdeaCategory.class).hasSize(expectedCategories.size())
+                .returnResult().getResponseBody();
+
+        //then
+        assertEquals(expectedCategories, actualCategories);
+
+    }
+
+    @Test
+    void shouldThrowNotFoundExceptionIfEntityWithProvidedIdDoesNotExist() {
+        //given
+        String notExistingId = "5000";
+        String expectedReason = "Idea with id %s does not exist".formatted(notExistingId);
+
+        //when
+        ErrorResponse actualResponse = webTestClient.get().uri("/api/idea/5000").exchange()
+                .expectStatus().isNotFound()
+                .expectBody(ErrorResponse.class)
+                .returnResult().getResponseBody();
+
+        //then
+        assertEquals(HttpStatus.NOT_FOUND, actualResponse.status());
+        assertEquals(1, actualResponse.reasons().size());
+        assertEquals(expectedReason, actualResponse.reasons().get(0));
 
     }
 
