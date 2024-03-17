e41991bc09b758eec3525675e91cc2f5585772ee
darch <damianarchala@gmail.com>
2024-03-18 00:06:23 +0100

    Update tests to clean context after each test

--- src/test/java/pl/archala/ideal/controller/IdeasControllerTest.java
+++ src/test/java/pl/archala/ideal/controller/IdeasControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import pl.archala.ideal.PostgresqlContainer;
 import pl.archala.ideal.dto.idea.AddIdeaDTO;
@@ -11,12 +12,13 @@ import pl.archala.ideal.dto.idea.GetIdeaDTO;
 import pl.archala.ideal.enums.IdeaCategory;
 import pl.archala.ideal.error.ErrorResponse;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class IdeasControllerTest extends PostgresqlContainer {
 
     @Autowired
@@ -28,7 +30,6 @@ class IdeasControllerTest extends PostgresqlContainer {
         String title = "idea-title";
         String content = "idea-content";
         AddIdeaDTO addIdeaDTO = new AddIdeaDTO(title, content);
-        GetIdeaDTO expectedPostResponse = new GetIdeaDTO(1L, title, content, LocalDateTime.now());
 
         //when
         GetIdeaDTO actualPostResponse = webTestClient.post().uri("/api/idea").bodyValue(addIdeaDTO).exchange()
@@ -36,17 +37,18 @@ class IdeasControllerTest extends PostgresqlContainer {
                 .expectBody(GetIdeaDTO.class)
                 .returnResult().getResponseBody();
 
-        GetIdeaDTO actualGetResponse = webTestClient.get().uri("api/idea/1").exchange()
+        GetIdeaDTO actualGetResponse = webTestClient.get().uri("api/idea/details/1").exchange()
                 .expectStatus().isOk()
                 .expectBody(GetIdeaDTO.class)
                 .returnResult().getResponseBody();
 
         //then
-        assertEquals(expectedPostResponse.id(), actualPostResponse.id());
-        assertEquals(expectedPostResponse.title(), actualPostResponse.title());
-        assertEquals(expectedPostResponse.content(), actualPostResponse.content());
+        assertEquals(1L, actualPostResponse.id());
+        assertEquals(title, actualPostResponse.title());
+        assertEquals(content, actualPostResponse.content());
 
         assertEquals(actualPostResponse, actualGetResponse);
+
     }
 
     @Test
@@ -56,7 +58,7 @@ class IdeasControllerTest extends PostgresqlContainer {
         String expectedReason = "Idea with id %s does not exist".formatted(notExistingId);
 
         //when
-        ErrorResponse actualResponse = webTestClient.get().uri("/api/idea/{id}", notExistingId).exchange()
+        ErrorResponse actualResponse = webTestClient.get().uri("/api/idea/details/{id}", notExistingId).exchange()
                 .expectStatus().isNotFound()
                 .expectBody(ErrorResponse.class)
                 .returnResult().getResponseBody();
@@ -74,13 +76,37 @@ class IdeasControllerTest extends PostgresqlContainer {
         List<IdeaCategory> expectedCategories = List.of(IdeaCategory.values());
 
         //when
-        List<IdeaCategory> actualCategories = webTestClient.get().uri("/api/idea/categories").exchange()
+        //then
+        webTestClient.get().uri("/api/idea/categories").exchange()
                 .expectStatus().isOk()
-                .expectBodyList(IdeaCategory.class).hasSize(expectedCategories.size())
+                .expectBodyList(IdeaCategory.class)
+                .isEqualTo(expectedCategories);
+
+    }
+
+    @Test
+    void shouldReturnIdeasPage() {
+        //given
+        AddIdeaDTO addIdeaDTO1 = new AddIdeaDTO("title-1", "content-1");
+        AddIdeaDTO addIdeaDTO2 = new AddIdeaDTO("title-2", "content-2");
+
+        //when
+        //then
+        GetIdeaDTO getIdeaDTO1 = webTestClient.post().uri("api/idea").bodyValue(addIdeaDTO1).exchange()
+                .expectStatus().isCreated()
+                .expectBody(GetIdeaDTO.class)
                 .returnResult().getResponseBody();
 
-        //then
-        assertEquals(expectedCategories, actualCategories);
+        GetIdeaDTO getIdeaDTO2 = webTestClient.post().uri("api/idea").bodyValue(addIdeaDTO2).exchange()
+                .expectStatus().isCreated()
+                .expectBody(GetIdeaDTO.class)
+                .returnResult().getResponseBody();
+
+        webTestClient.get().uri("api/idea/page").exchange()
+                .expectStatus().isOk()
+                .expectBodyList(GetIdeaDTO.class).hasSize(2)
+                .contains(getIdeaDTO1, getIdeaDTO2);
+
     }
 
 }
