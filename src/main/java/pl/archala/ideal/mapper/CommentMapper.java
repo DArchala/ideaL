038a85a2f95db14bf0690069610d1ea63e1a6bee
darch <damianarchala@gmail.com>
2024-03-20 17:15:05 +0100

    Update CommentMapper

--- src/main/java/pl/archala/ideal/mapper/CommentMapper.java
+++ src/main/java/pl/archala/ideal/mapper/CommentMapper.java
@@ -1,9 +1,7 @@
 package pl.archala.ideal.mapper;
 
 import org.springframework.stereotype.Component;
-import pl.archala.ideal.dto.comment.AddCommentCommentDTO;
-import pl.archala.ideal.dto.comment.AddIdeaCommentDTO;
-import pl.archala.ideal.dto.comment.AddRealizationCommentDTO;
+import pl.archala.ideal.dto.comment.AddCommentDTO;
 import pl.archala.ideal.dto.comment.GetCommentDTO;
 import pl.archala.ideal.entity.Comment;
 import pl.archala.ideal.utils.IdealLocalDateTime;
@@ -11,23 +9,9 @@ import pl.archala.ideal.utils.IdealLocalDateTime;
 @Component
 public class CommentMapper {
 
-    public Comment toEntity(AddIdeaCommentDTO commentDTO) {
+    public Comment toEntity(AddCommentDTO addCommentDTO) {
         Comment comment = new Comment();
-        comment.setContent(commentDTO.content());
-        comment.setCreated(IdealLocalDateTime.now());
-        return comment;
-    }
-
-    public Comment toEntity(AddCommentCommentDTO commentDTO) {
-        Comment comment = new Comment();
-        comment.setContent(commentDTO.content());
-        comment.setCreated(IdealLocalDateTime.now());
-        return comment;
-    }
-
-    public Comment toEntity(AddRealizationCommentDTO addRealizationCommentDTO) {
-        Comment comment = new Comment();
-        comment.setContent(addRealizationCommentDTO.content());
+        comment.setContent(addCommentDTO.content());
         comment.setCreated(IdealLocalDateTime.now());
         return comment;
     }
