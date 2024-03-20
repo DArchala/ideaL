5373f84a75bc7de7fbce9daf04e484598f8c23e7
darch <damianarchala@gmail.com>
2024-03-20 17:22:09 +0100

    Update CommentMapperTest

--- src/test/java/pl/archala/ideal/mapper/CommentMapperTest.java
+++ src/test/java/pl/archala/ideal/mapper/CommentMapperTest.java
@@ -1,62 +1,29 @@
 package pl.archala.ideal.mapper;
 
 import org.junit.jupiter.api.Test;
-import pl.archala.ideal.dto.comment.AddCommentCommentDTO;
-import pl.archala.ideal.dto.comment.AddIdeaCommentDTO;
-import pl.archala.ideal.dto.comment.AddRealizationCommentDTO;
+import pl.archala.ideal.dto.comment.AddCommentDTO;
 import pl.archala.ideal.entity.Comment;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CommentMapperTest {
 
     private final CommentMapper commentMapper = new CommentMapper();
 
     @Test
-    void shouldReturnCommentFromAddIdeaCommentDTO() {
+    void shouldReturnCommentByAddCommentDTO() {
         //given
-        AddIdeaCommentDTO addIdeaCommentDTO = new AddIdeaCommentDTO("simple-content", 1L);
+        AddCommentDTO addCommentDTO = new AddCommentDTO("simple-content", 1L);
 
         Comment expectedComment = new Comment();
-        expectedComment.setContent(addIdeaCommentDTO.content());
+        expectedComment.setContent(addCommentDTO.content());
 
         //when
-        Comment actualComment = commentMapper.toEntity(addIdeaCommentDTO);
+        Comment actualComment = commentMapper.toEntity(addCommentDTO);
         expectedComment.setCreated(actualComment.getCreated());
 
         //then
         assertEquals(expectedComment, actualComment);
     }
 
-    @Test
-    void shouldReturnCommentFromAddRealizationCommentDTO() {
-        //given
-        AddRealizationCommentDTO addRealizationCommentDTO = new AddRealizationCommentDTO("simple-content", 1L);
-
-        Comment expectedComment = new Comment();
-        expectedComment.setContent(addRealizationCommentDTO.content());
-
-        //when
-        Comment actualComment = commentMapper.toEntity(addRealizationCommentDTO);
-        expectedComment.setCreated(actualComment.getCreated());
-
-        //then
-        assertEquals(expectedComment, actualComment);
-    }
-
-    @Test
-    void shouldReturnCommentFromAddCommentCommentDTO() {
-        //given
-        AddCommentCommentDTO addCommentCommentDTO = new AddCommentCommentDTO("simple-content", 1L);
-
-        Comment expectedComment = new Comment();
-        expectedComment.setContent(addCommentCommentDTO.content());
-
-        //when
-        Comment actualComment = commentMapper.toEntity(addCommentCommentDTO);
-        expectedComment.setCreated(actualComment.getCreated());
-
-        //then
-        assertEquals(expectedComment, actualComment);
-    }
 }
