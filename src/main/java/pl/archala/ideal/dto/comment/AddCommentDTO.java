5491c5dbd855420ac79b8be380b2dbd2a4494023
darch <damianarchala@gmail.com>
2024-03-20 17:14:39 +0100

    Merge all add comment dto to one

--- src/main/java/pl/archala/ideal/dto/comment/AddCommentDTO.java
+++ src/main/java/pl/archala/ideal/dto/comment/AddCommentDTO.java
@@ -3,7 +3,7 @@ package pl.archala.ideal.dto.comment;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record AddCommentCommentDTO(
+public record AddCommentDTO(
         @NotBlank(message = "Comment content must not be blank") String content,
-        @NotNull Long parentCommentId) {
+        @NotNull Long parentId) {
 }
--- src/main/java/pl/archala/ideal/dto/comment/AddRealizationCommentDTO.java
+++
@@ -1,9 +0,0 @@
-package pl.archala.ideal.dto.comment;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record AddRealizationCommentDTO(
-        @NotBlank(message = "Comment content must not be blank") String content,
-        @NotNull Long realizationId) {
-}
